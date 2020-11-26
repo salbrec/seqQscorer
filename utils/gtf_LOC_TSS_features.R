a50b7ecf9a48788ea48e8236df6254fdd2743f98
steffen <salbrec@uni-mainz.de>
2020-11-26 16:51:39 +0100

    add datasets

---
+++ utils/gtf_LOC_TSS_features.R
@@ -0,0 +1,41 @@
+library(GenomicFeatures)
+library(ChIPpeakAnno)
+library(ChIPseeker)
+
+args <- commandArgs(trailingOnly = TRUE)
+
+bed_file = args[1]
+gtf_file = args[2]
+out_file_base = args[3]
+
+# create TxDb from given gtf file
+custom_TxDb = makeTxDbFromGFF(gtf_file)
+
+
+# run the TSS annotaions to derive the TSS features
+reads <- toGRanges(bed_file, format="BED", header=FALSE)
+
+annoDataTSS <- toGRanges(custom_TxDb, feature="gene")
+
+tssAnno = binOverFeature(reads, annotationData=annoDataTSS, radius=5000, nbins=5, select='nearest', FUN=length)
+tssAnno = DataFrame(tssAnno)
+
+tssAnno$tss_dist = as.numeric(rownames(tssAnno))
+tssAnno$perc = tssAnno$reads / length(reads) * 100
+
+tss_out = paste(out_file_base, ".TSS", sep="")
+write.table(tssAnno,file=tss_out, sep="\t", row.names = FALSE)
+
+
+# run the peak annotation to dreive the LOC features
+seqlevelsStyle(reads) <- "NCBI"
+readsAnno = annotatePeak(reads, tssRegion=c(-1000, 150), TxDb=custom_TxDb, verbose=TRUE)
+
+percentages = data.frame(show(readsAnno))
+
+loc_out = paste(out_file_base, ".LOC", sep="")
+write.table(percentages,file=loc_out, sep="\t")
+
+
+
+
